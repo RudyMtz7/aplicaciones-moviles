f5300c8bc0fea4bd103326a695987361dd19a2b7
Rodolfo <rudy@brounie.com>
2019-03-04 19:11:38 -0600

    Añadir Expo

---
+++ Expo/HelloWorld/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello World! 乁(ʘ◡ʘ)ㄏ </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white'
+  },
+});
